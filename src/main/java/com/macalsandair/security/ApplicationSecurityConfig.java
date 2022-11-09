b9fce7ad7661244492016115d02c299705458527
MacAlsandair <mac.alsandair@gmail.com>
2022-11-09 12:18:18 +0200

    I have added EnableGlobalSecurity annotation, that now @PreAuthorize should work

--- src/main/java/com/macalsandair/security/ApplicationSecurityConfig.java
+++ src/main/java/com/macalsandair/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,6 +23,7 @@ import static com.macalsandair.security.ApplicationUserPermission.*;
 @SuppressWarnings("deprecation")
 @Configuration 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private final PasswordEncoder passwordEncoder;
