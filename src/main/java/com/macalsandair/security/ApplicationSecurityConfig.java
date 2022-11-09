d19b2ce99b06c7fa6e241f9f60e6eaab06c709a5
MacAlsandair <mac.alsandair@gmail.com>
2022-11-09 13:07:33 +0200

    I have added a login page

--- src/main/java/com/macalsandair/security/ApplicationSecurityConfig.java
+++ src/main/java/com/macalsandair/security/ApplicationSecurityConfig.java
@@ -49,7 +49,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest()
 			.authenticated()
 			.and()
-			.formLogin();
+			.formLogin()
+			.loginPage("/login").permitAll();
 	}
 
 	@Override
